bf243c18840a5d3ef805e76ef5783a1c710a14fd
M4rI0 <98322013+M4rI0o@users.noreply.github.com>
2023-01-14 08:52:38 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,53 +1,86 @@
-import random
+import os
+import sys
 
-import pygame as pg
-from pygame.math import Vector2
+import pygame as pygame
+
+pygame.init()
+width = 850
+height = 550
+size = 850, 550
+screen = pygame.display.set_mode(size)
 
 
-class Projectile(pg.sprite.Sprite):
+############################ Загрузка и обработка изображения ################################
+def load_image(name, colorkey=None):
+    fullname = os.path.join('backside', name)
+    # если файл не существует, то выходим
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    return image
 
-    def __init__(self, pos, game_area):
-        super().__init__()
-        self.image = pg.Surface((5, 5))
-        self.image.fill(pg.Color('aquamarine2'))
-        self.rect = self.image.get_rect(center=pos)
-        self.vel = Vector2(2, 0).rotate(random.randrange(360))
-        self.pos = Vector2(pos)
-        self.game_area = game_area
+
+fon = pygame.transform.scale(load_image('back.jpg'), (width, height))
+screen.blit(fon, (0, 0))
+
+###################################### Создаем меню ############################################
+
+Arial = pygame.font.SysFont('arial', 45)
+
+
+class Menu:
+    def __init__(self):
+        self.x = None
+        self.option_surfaces = []
+        self.callbacks = []
+        self.last_option_index = 0
+
+    def append_option(self, sth_new, callback):
+        self.option_surfaces.append(Arial.render(sth_new, True, 'White'))
+        self.callbacks.append(callback)
+
+    def switch(self, direction):
+        self.last_option_index = max(0, min(self.last_option_index + direction, len(self.option_surfaces) - 1))
+
+    def select(self):
+        self.callbacks[self.last_option_index]()
+
+    def draw(self, surface, x, y, padding):
+        self.x = x
+        for index, option in enumerate(self.option_surfaces):
+            option_rect = option.get_rect()
+            option_rect.topleft = (self.x, y + index * padding)
+            if index == self.last_option_index:
+                pygame.draw.rect(surface, 'black', option_rect, 3)
+            else:
+                pygame.draw.rect(surface, 'grey', option_rect, 1)
+            surface.blit(option, option_rect)
 
     def update(self):
-        self.pos += self.vel
-        self.rect.center = self.pos
-        if not self.game_area.contains(self.rect):
-            self.kill()
 
 
-def main():
-    screen = pg.display.set_mode((640, 480))
-    game_area = pg.Rect(60, 60, 520, 360)
-    game_area_color = pg.Color('aquamarine2')
-    clock = pg.time.Clock()
-    all_sprites = pg.sprite.Group(Projectile(game_area.center, game_area))
 
-    done = False
+menu = Menu()
+menu.append_option('Начать игру', lambda: print('heu'))
+menu.append_option('выход', quit)
 
-    while not done:
-        for event in pg.event.get():
-            if event.type == pg.QUIT:
-                done = True
+##################################### Основной игровой цикл #####################################
+running = True
 
-        all_sprites.add(Projectile(game_area.center, game_area))
-        all_sprites.update()
-
-        screen.fill((30, 30, 30))
-        all_sprites.draw(screen)
-        pg.draw.rect(screen, game_area_color, game_area, 2)
-
-        pg.display.flip()
-        clock.tick(60)
-
-
-if __name__ == '__main__':
-    pg.init()
-    main()
-    pg.quit()
+while running:
+    x = 320
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                menu.switch(-1)
+            elif event.key == pygame.K_DOWN:
+                menu.switch(1)
+            elif event.key == pygame.K_RETURN:
+                menu.select()
+                x += 700
+    menu.draw(screen, x, 300, 50)
+    pygame.display.flip()
+quit()
